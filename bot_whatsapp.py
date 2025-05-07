0a953463da705f1dc8adeb6165ca61939d595a45
Franc Washington Vilela <fvilela216@gmail.com>
2025-05-07 15:36:38 -0300

    Ajusta importação do dialogflow para v2

--- bot_whatsapp.py
+++ bot_whatsapp.py
@@ -1,7 +1,8 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-from google.cloud import dialogflow
+from google.cloud import dialogflow_v2 as dialogflow
 import os
+from google.cloud.dialogflow_v2 import types
 
 app = Flask(__name__)
 
@@ -19,8 +20,8 @@ SESSION_ID = 'current-user-id'
 def detect_intent_texts(project_id, session_id, text, language_code):
     session_client = dialogflow.SessionsClient(credentials=credentials)
     session = session_client.session_path(project_id, session_id)
-    text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
-    query_input = dialogflow.types.QueryInput(text=text_input)
+    text_input = types.TextInput(text=text, language_code=language_code)
+    query_input = types.QueryInput(text=text_input)
     response = session_client.detect_intent(session=session, query_input=query_input)
     return response.query_result.fulfillment_text
 
